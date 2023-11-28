ffcee7ce87c05c39138fb721fd62c1ea7fecf0e0
Atef Ataya <atef.ataya>
2023-11-28 19:56:59 +0400

    Finishing Domain Core Module

---
+++ WebApi.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Domain.Common;
+
+namespace WebApi.Domain.Entities
+{
+    public class Order : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
