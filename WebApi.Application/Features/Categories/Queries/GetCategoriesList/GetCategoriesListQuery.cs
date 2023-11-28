33343524dfda5129ff1026c4e5fa85bfef4c9604
Atef Ataya <atef.ataya>
2023-11-28 20:42:18 +0400

    Little Enhancement

--- WebApi.Application/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQuery.cs
+++ WebApi.Application/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQuery.cs
@@ -4,10 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApi.Application.Contracts.Caching;
 
 namespace WebApi.Application.Features.Categories.Queries.GetCategoriesList
 {
-    public class GetCategoriesListQuery : IRequest<List<CategoryListVm>>
+    public class GetCategoriesListQuery : IRequest<List<CategoryListVm>>, ICacheableMediatrQuery
     {
+        public bool BypassCache { get; set; }
+        public string CacheKey => $"CategoryList";
+        public TimeSpan? SlidingExpiration { get; set; }
     }
 }
