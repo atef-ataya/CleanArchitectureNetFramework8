b5635f7821f99beff900d03db38b27be93f830ec
Atef Ataya <atef.ataya>
2023-11-28 20:18:40 +0400

    Adding Caching behaviors for the Application Module

---
+++ WebApi.Application/Contracts/Caching/ICacheableMediatrQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.Contracts.Caching
+{
+    public interface ICacheableMediatrQuery
+    {
+        bool BypassCache { get; }
+        string CacheKey { get; }
+        TimeSpan? SlidingExpiration { get; }
+    }
+}
