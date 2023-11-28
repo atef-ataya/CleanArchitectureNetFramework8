9f339a84cbeeb04101d475f84b907c27396df45d
Atef Ataya <atef.ataya>
2023-11-28 21:31:00 +0400

    Adding Infrastructure Module: Identity

---
+++ WebApi.Identity/WebApiIdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Identity.Models;
+
+namespace WebApi.Identity
+{
+    public class WebApiIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public WebApiIdentityDbContext(DbContextOptions<WebApiIdentityDbContext> options) : base(options) { }
+    }
+}
