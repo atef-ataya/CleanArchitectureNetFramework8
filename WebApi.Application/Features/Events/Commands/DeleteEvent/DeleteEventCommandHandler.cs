43eedf294f7d954bfa06fb0049533f86c2c1f067
Atef Ataya <atef.ataya>
2023-11-28 21:10:58 +0400

    Adding: Delete Event Update Event

---
+++ WebApi.Application/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Contracts.Persistence;
+using WebApi.Application.Exceptions;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Application.Features.Events.Commands.DeleteEvent
+{
+    public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteEventCommandHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventToDelete = await _eventRepository.GetByIdAsync(request.EventId);
+
+            if (eventToDelete == null)
+            {
+                throw new NotFoundException(nameof(Event), request.EventId);
+            }
+
+            await _eventRepository.DeleteAsync(eventToDelete);
+        }
+    }
+
+}
---
+++ WebApi.Application/Features/Events/Commands/UpdateEvent/UpdateEventCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Contracts.Persistence;
+using WebApi.Application.Exceptions;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Application.Features.Events.Commands.UpdateEvent
+{
+    public class UpdateEventCommandHandler : IRequestHandler<UpdateEventCommand>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateEventCommandHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task Handle(UpdateEventCommand request, CancellationToken cancellationToken)
+        {
+
+            var eventToUpdate = await _eventRepository.GetByIdAsync(request.EventId);
+
+            if (eventToUpdate == null)
+            {
+                throw new NotFoundException(nameof(Event), request.EventId);
+            }
+
+            var validator = new UpdateEventCommandValidator();
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Count > 0)
+                throw new ValidationException(validationResult);
+
+            _mapper.Map(request, eventToUpdate, typeof(UpdateEventCommand), typeof(Event));
+
+            await _eventRepository.UpdateAsync(eventToUpdate);
+        }
+
+      
+    }
+
+}
